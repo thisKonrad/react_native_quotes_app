1382e759fd7d006a61b873d2f412ee048c22bf05
this.Konrad <konradlebt@gmail.com>
2024-02-12 14:47:19 +0100

    basic styles for quotes and author

--- components/Quotes/index.js
+++ components/Quotes/index.js
@@ -1,23 +1,42 @@
 /* :::: QUOTES :::: */
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 
 export default function Quotes({ text, author }) {
 
-    return (<>
+    return (<View style={styles.quoteWrap}>
         <Text style={styles.quoteText}>{text}</Text>
-        <Text>&mdash; {author}</Text>
-    </>)
+        <Text style={styles.authorText}>&mdash; {author}</Text>
+    </View>)
 
 }
 
 const styles = StyleSheet.create({
+    quoteWrap: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        gap: '1rem',
+        width: '90%',
+        paddingHorizontal: '4%',
+        marginBottom: 45,
+    },
     quoteText: {
+        textAlign: 'justify',
         color: 'white',
-        fontWeight: '900',
+        fontStyle: 'italic',
+        fontSize: 32,
+        letterSpacing: '2px',
+        lineHeight: '2.4rem',
+        fontWeight: '600',
+        marginBottom: 6,
     },
     authorText: {
+        textAlign: 'right',
         color: 'white',
-        fontWeight: '600',
+        fontSize: 16,
+        fontWeight: '400',
+
     }
 });
