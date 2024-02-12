cc9a0d933abfd6c30c56dad6849009b1baa5720a
this.Konrad <konradlebt@gmail.com>
2024-02-12 11:10:34 +0100

    add next button and array of dummy quotes

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const data = [
+  { text: "Ambition is a dream with an V8 engine", author: "Elvis Presley" },
+  { text: "Erzähle mir nicht wie meine Chancen stehen", author: "Han Solo" },
+  { text: "Hendrix tat genau was ich wollte, ich konnte es nur nicht", author: "Jeff Beck" },
+];
+
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi Mom!</Text>
+      <Text style={styles.text}>Greed has taken the whole universe, and nobody is worried about thier soul</Text>
+      <Text>--Little Richard</Text>
+      <Button
+        title="next"
+        onPress={() => alert('Hi Mom!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
